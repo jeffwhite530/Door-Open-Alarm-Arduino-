d6b423e841cad590957bca6b2e2443dd48c3ab4b
Jeff White <44498114+jeffwhite530@users.noreply.github.com>
2018-10-26 07:56:28 -0400

    Adding initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+Door Open Alarm (Arduino)
+======
+
+This is a simple Arduino project to create a device to make an audible alarm when a door (or window, or refridgerator, etc.) has been left open.
+
+This was created as a learning project and as such the code and design should not be trusted.
+
---
+++ door_open_alarm/door_open_alarm.ino
@@ -0,0 +1,65 @@
+const int pinSwitch = 12; // Reed
+const int pinRedLed = 11; // Red LED
+const int pinGreenLed = 10; // Green LED
+const int pinBuzzer = 9; // Buzzer
+
+int StatoSwitch = 0;
+
+
+
+void setup() {
+  pinMode(pinSwitch, INPUT);
+  pinMode(pinRedLed, OUTPUT);
+  pinMode(pinGreenLed, OUTPUT);
+  pinMode(pinBuzzer, OUTPUT);
+}
+
+
+
+void beepBuzzer() {
+  int secondsCounted = 0;
+  int secondsToWait = 30;
+
+  // Wait N seconds before buzzing or exit if the switch closes within that timer limit
+  while (secondsCounted < secondsToWait) {
+    StatoSwitch = digitalRead(pinSwitch);
+    
+    if (StatoSwitch == LOW) {
+      secondsCounted++;
+      delay(1000);
+    }
+    else {
+      return;
+    }
+  }
+
+  // BEEP BEEP BEEP ... until the switch closes
+  while (StatoSwitch == LOW) {
+    tone(pinBuzzer, 250);
+    delay(500);
+    noTone(pinBuzzer);
+    delay(500);
+
+    StatoSwitch = digitalRead(pinSwitch);
+  }
+}
+
+
+
+void loop() {
+  StatoSwitch = digitalRead(pinSwitch);
+
+  // Green LED when the switch is closed
+  if (StatoSwitch == HIGH) {
+    digitalWrite(pinGreenLed, HIGH);
+    digitalWrite(pinRedLed, LOW);
+    noTone(pinBuzzer);
+  }
+
+  // Red LED and beeping buzzer when the switch is open
+  else {
+    digitalWrite(pinGreenLed, LOW);
+    digitalWrite(pinRedLed, HIGH);
+    beepBuzzer();
+  }
+}
