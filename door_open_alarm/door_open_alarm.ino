72cbdf0d3de189fb1c6cd4ecfba2370ad1490ca3
Jeff White <44498114+jeffwhite530@users.noreply.github.com>
2018-10-26 12:42:58 -0400

    Fixing tabs

--- door_open_alarm/door_open_alarm.ino
+++ door_open_alarm/door_open_alarm.ino
@@ -8,58 +8,58 @@ int StatoSwitch = 0;
 
 
 void setup() {
-  pinMode(pinSwitch, INPUT);
-  pinMode(pinRedLed, OUTPUT);
-  pinMode(pinGreenLed, OUTPUT);
-  pinMode(pinBuzzer, OUTPUT);
+	pinMode(pinSwitch, INPUT);
+	pinMode(pinRedLed, OUTPUT);
+	pinMode(pinGreenLed, OUTPUT);
+	pinMode(pinBuzzer, OUTPUT);
 }
 
 
 
 void beepBuzzer() {
-  int secondsCounted = 0;
-  int secondsToWait = 30;
+	int secondsCounted = 0;
+	int secondsToWait = 30;
 
-  // Wait N seconds before buzzing or exit if the switch closes within that timer limit
-  while (secondsCounted < secondsToWait) {
-    StatoSwitch = digitalRead(pinSwitch);
-    
-    if (StatoSwitch == LOW) {
-      secondsCounted++;
-      delay(1000);
-    }
-    else {
-      return;
-    }
-  }
+	// Wait N seconds before buzzing or exit if the switch closes within that timer limit
+	while (secondsCounted < secondsToWait) {
+		StatoSwitch = digitalRead(pinSwitch);
+		
+		if (StatoSwitch == LOW) {
+			secondsCounted++;
+			delay(1000);
+		}
+		else {
+			return;
+		}
+	}
 
-  // BEEP BEEP BEEP ... until the switch closes
-  while (StatoSwitch == LOW) {
-    tone(pinBuzzer, 250);
-    delay(500);
-    noTone(pinBuzzer);
-    delay(500);
+	// BEEP BEEP BEEP ... until the switch closes
+	while (StatoSwitch == LOW) {
+		tone(pinBuzzer, 250);
+		delay(500);
+		noTone(pinBuzzer);
+		delay(500);
 
-    StatoSwitch = digitalRead(pinSwitch);
-  }
+		StatoSwitch = digitalRead(pinSwitch);
+	}
 }
 
 
 
 void loop() {
-  StatoSwitch = digitalRead(pinSwitch);
+	StatoSwitch = digitalRead(pinSwitch);
 
-  // Green LED when the switch is closed
-  if (StatoSwitch == HIGH) {
-    digitalWrite(pinGreenLed, HIGH);
-    digitalWrite(pinRedLed, LOW);
-    noTone(pinBuzzer);
-  }
+	// Green LED when the switch is closed
+	if (StatoSwitch == HIGH) {
+		digitalWrite(pinGreenLed, HIGH);
+		digitalWrite(pinRedLed, LOW);
+		noTone(pinBuzzer);
+	}
 
-  // Red LED and beeping buzzer when the switch is open
-  else {
-    digitalWrite(pinGreenLed, LOW);
-    digitalWrite(pinRedLed, HIGH);
-    beepBuzzer();
-  }
+	// Red LED and beeping buzzer when the switch is open
+	else {
+		digitalWrite(pinGreenLed, LOW);
+		digitalWrite(pinRedLed, HIGH);
+		beepBuzzer();
+	}
 }
